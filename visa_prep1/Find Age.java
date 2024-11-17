02d1de1f665b050942a0a2f60fa727ce883a5ea0
VaishnaviBasa <vaishnavibasa025@gmail.com>
2024-11-17 19:45:38 +0530

    Create Find Age.java

---
+++ visa_prep1/Find Age.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        System.out.println(10 + (X - 25));
+        scanner.close();
+    }
+}
