eade504c69c290db3c68a37ea33e3302fa891c16
VaishnaviBasa <vaishnavibasa025@gmail.com>
2024-11-18 19:07:24 +0530

    Create Matrix Sum.java

---
+++ visa_prep1/Matrix Sum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int rowSum = 0, colSum = 0;
+            for (int j = 0; j < n; j++) {
+                rowSum += matrix[i][j];
+                colSum += matrix[j][i];
+            }
+            result[i] = rowSum + colSum;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(result[i] + (i < n - 1 ? " " : ""));
+        }
+    }
+}
